c782e23d7608a231030970dd3e21961f912368f3
Dharitri Dixit <dharidix@gmail.com>
2021-09-30 12:05:14 -0400

    Updates

---
+++ precache-manifest.7c1a8b2d225f42ab3456ebb61a733b48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2d4982487213ba7c5a1f1c4154076e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0e12f8353efa3d490472",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "fd9ae3fa217f6e678dc4",
+    "url": "/home/static/js/2.fffcefba.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.fffcefba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e12f8353efa3d490472",
+    "url": "/home/static/js/main.7a584797.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "d03e19d946fbb94f8a56091147565aa8",
+    "url": "/home/static/media/SummerResume.d03e19d9.pdf"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99bd0547524acc9097866401dd3cab61.js"
+  "/home/precache-manifest.7c1a8b2d225f42ab3456ebb61a733b48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
